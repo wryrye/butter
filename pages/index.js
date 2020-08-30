51351df0b7545fb375d00840d6861519453c4868
Ryan Coughlin <ryan.coughlin@washpost.com>
2020-08-30 16:51:13 -0400

    update title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>B U T T E R</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
